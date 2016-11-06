3ed0c928562864bfe00ae4dd18bdb91a979d9c15
Brandon Garling <bjg96@nau.edu>
2016-11-05 18:56:25 -0700

    Add favicon

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(users);
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
