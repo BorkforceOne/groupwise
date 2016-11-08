e888f11e50d46ef8f2018212e986f363fa9afe92
Brandon Garling <bjg96@nau.edu>
2016-11-07 21:47:04 -0700

    Hash the user's password rather than storing the password raw

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@
  */
 const Sequelize = require('sequelize');
 const database = require('../user_modules/database');
+const encryption = require('../user_modules/encryption');
 
 module.exports = {};
 
@@ -27,11 +28,28 @@ const User = database.sequelize.define('user', {
   Password: {
     allowNull: false,
     type: Sequelize.STRING
+  },
+  Salt: {
+    allowNull: false,
+    type: Sequelize.STRING
   }
 },{
   instanceMethods: {
     getSerializableFields: function() {
       return User.getSerializableFields();
+    },
+    changePassword: function(password) {
+      return new Promise(function (resolve, reject) {
+        encryption.generateSalt()
+          .then(function (salt) {
+            this['Salt'] = salt;
+            encryption.generateHash(password, salt)
+              .then(function (hash) {
+                this['Password'] = hash;
+                resolve(true);
+              }.bind(this));
+          }.bind(this));
+      }.bind(this));
     }
   },
 });
--- server/routes/users.js
+++ server/routes/users.js
@@ -22,11 +22,24 @@ router.get('/api/v1/users', function(req, res, next) {
 /* Add a new user */
 router.post('/api/v1/users', function(req, res, next) {
   res.type('json');
-  User.create(req.body)
-    .then(function (user) {
-      let serialized = serializer.serializeModel(user);
-      let response = restUtils.prepareResponse(serialized);
-      res.send(JSON.stringify(response));
+  let workingUser = User.build();
+
+  workingUser['Firstname'] = req.body.Firstname;
+  workingUser['Lastname'] = req.body.Lastname;
+  workingUser['Email'] = req.body.Email;
+  workingUser.changePassword(req.body.Password)
+    .then(function () {
+      workingUser.save()
+        .then(function (user) {
+          let serialized = serializer.serializeModel(user);
+          let response = restUtils.prepareResponse(serialized);
+          res.send(JSON.stringify(response));
+        })
+        .catch(function (err) {
+          console.log("Error: ", err);
+          let response = restUtils.prepareResponse({}, ["There was an error processing the request"]);
+          res.send(JSON.stringify(response));
+        });
     })
     .catch(function (err) {
       console.log("Error: ", err);
