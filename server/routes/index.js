9dff1f4ba8e8af4591f55871e3daf6f3d0a8c77b
Brandon Garling <bjg96@nau.edu>
2017-02-08 01:32:41 -0700

    Automatically require the correct files based on filename

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,13 @@
-const _ = require('lodash');
 const router = require('express').Router();
 
-const usersRoute = require('./users.route');
-const authsRoute = require('./auths.route');
-const attachmentsRoute = require('./attachments.routes');
-const configsRoute = require('./configs.route');
-const tokensRoute = require('./tokens.route.js');
-const hostProfilesRoute = require('./host-profiles.route');
-const studentProfilesRoute = require('./student-profiles.route');
+let normalizedPath = require("path").join(__dirname);
 
-router.use('/', usersRoute, authsRoute, attachmentsRoute, configsRoute, tokensRoute, hostProfilesRoute, studentProfilesRoute);
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  if (file.match(/\.route\.js/)) {
+    let route = require("./" + file);
+    console.log("[ROUTES] Loading route '" + file + "'");
+    router.use('/', route);
+  }
+});
 
 module.exports = router;
