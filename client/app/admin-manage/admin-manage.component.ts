ed84365890150a567bd4cbb520b50a0085e5983f
Matt <mrn24@nau.edu>
2017-02-20 10:58:36 -0700

    Added table for removing attributes and a basic form for creating. Still need to write submit functions and remove buttons.

--- client/app/admin-manage/admin-manage.component.ts
+++ client/app/admin-manage/admin-manage.component.ts
@@ -4,6 +4,9 @@ import {ConfigService} from "../services/config/config.service";
 import {Observable} from "rxjs";
 import {User} from "../services/user/user";
 import {UserService} from "../services/user/user.service";
+import {AttributeService} from "../services/attributes/attribute.service";
+import {Attribute} from "../services/attributes/attribute.model";
+import {AttributeString} from "../services/attributes/attribute-string.model";
 
 @Component({
   selector: 'app-admin-manage',
@@ -14,9 +17,16 @@ export class AdminManageComponent implements OnInit {
   private URL: string = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/api/v1/attachments";
   private uploader:FileUploader;
   private users: User[];
+  private attributes: Attribute[] = [];
 
 
-  constructor(private configService: ConfigService, private userService: UserService) { }
+  constructor(private configService: ConfigService, private userService: UserService, private attributeService: AttributeService) {
+    this.attributeService.getAllAttributes()
+      .subscribe(attributes => {
+        this.attributes = attributes
+        console.log(attributes);
+      });
+  }
 
   ngOnInit() {
     this.uploader = new FileUploader({url: this.URL});
@@ -34,6 +44,7 @@ export class AdminManageComponent implements OnInit {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
     });
+
   }
 
 }
