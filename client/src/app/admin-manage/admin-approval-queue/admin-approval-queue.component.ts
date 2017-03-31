3506126d90260ea6da7492fef17df4004964f897
Matt <mrn24@nau.edu>
2017-03-31 15:47:53 -0700

    Created admin queue files

---
+++ client/src/app/admin-manage/admin-approval-queue/admin-approval-queue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-approval-queue',
+  templateUrl: './admin-approval-queue.component.html',
+  styleUrls: ['./admin-approval-queue.component.scss']
+})
+export class AdminApprovalQueueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
