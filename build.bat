cc8d41e449e0f8bcaf3f27afb391f3c4bde34683
Brandon Garling <bjg96@nau.edu>
2017-04-24 19:18:53 -0700

    Remove unneeded server files and ensure we don't compile for AOT

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rmdir /s /q "dist"
 cd client
-call ng build --prod
+call ng build --prod --aot false
 cd ..
 xcopy "client\dist" "dist\www\*" /s /e
 xcopy "server\templates" "dist\templates\*" /s /e
